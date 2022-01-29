0cee9641d5005c06dc9d145b10c5f8eeb98cdf01
ManuFrancisco <madofscott7@gmail.com>
2022-01-29 14:08:32 +0100

    First commit

---
+++ UtilizandoBeans/src/main/java/com/madofsys/dio/AutorLivro.java
@@ -0,0 +1,7 @@
+package com.madofsys.dio;
+
+public interface AutorLivro {
+	
+	void exibirAutor();
+
+}
---
+++ UtilizandoBeans/src/test/java/com/madofsys/dio/UtilizandoBeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madofsys.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilizandoBeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
