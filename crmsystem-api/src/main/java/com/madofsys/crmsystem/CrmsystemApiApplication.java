f34937510e3d62d1435e4054e84a3244d6bb1eed
ManuFrancisco <madofscott7@gmail.com>
2022-01-30 13:53:30 +0100

    my second commit

---
+++ crmsystem-api/src/main/java/com/madofsys/crmsystem/CrmsystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.madofsys.crmsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmsystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmsystemApiApplication.class, args);
+	}
+
+}
---
+++ crmsystem-api/src/test/java/com/madofsys/crmsystem/CrmsystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madofsys.crmsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmsystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
